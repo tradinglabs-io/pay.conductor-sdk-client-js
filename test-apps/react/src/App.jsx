e0c677fdd1dc3372853cd87d1c0f20e1cc7b7576
Wilssola <wilssola@icloud.com>
2026-02-16 10:36:14 -0300

    first commit

---
+++ test-apps/react/src/App.jsx
@@ -0,0 +1,13 @@
+import { AutoComplete, Todos } from '@pay.conductor-sdk-client-js/library-react';
+
+function App() {
+  return (
+    <div>
+      <div>Welcome to React.</div>
+      <AutoComplete />
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
