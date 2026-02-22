4406681d698a12eadb6301f68723436d9a547bf4
Wilssola <wilssola@icloud.com>
2026-02-22 00:55:45 -0300

    fix: iframe is ready global definition

--- library/v1/src/payconductor/payconductor.lite.tsx
+++ library/v1/src/payconductor/payconductor.lite.tsx
@@ -168,6 +168,20 @@ export default function PayConductor(props: PayConductorEmbedProps) {
 		};
 
 		window.addEventListener("message", eventHandler);
+
+		const setupIframeLoadListener = () => {
+			const el = getIframe();
+			if (!el) return false;
+			el.addEventListener("load", () => sendConfigToIframe(), { once: true });
+			return true;
+		};
+
+		if (!setupIframeLoadListener()) {
+			const iframeObserver = new MutationObserver(() => {
+				if (setupIframeLoadListener()) iframeObserver.disconnect();
+			});
+			iframeObserver.observe(document.body, { childList: true, subtree: true });
+		}
 	});
 
 	return (
